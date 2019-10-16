32df9af02493f607aa078752880a9fc96a7d9072
jamoragar <javier.moragarojas@gmail.com>
2019-10-16 16:15:29 -0300

    FIX - Footer with css problems. Now you can see everything ok in Cellphones

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,20 +13,21 @@ const Footer = () => {
 								<h3>Contáctanos</h3>
 								<p>Queremos resolver tus inquietudes.</p>
 								<a className="fb-ic" href='# '>
-									<i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> /Under-Patagonia</i>
+									<i className="fab fa-facebook-f fa-lg white-text mr-md-2"> /Under-Patagonia</i>
 								</a>
 								<a className="tw-ic" href='https://twitter.com/PatagoniaUnder' target='_blank' rel="noopener noreferrer">
-									<i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> @PatagoniaUnder</i>
+									<i className="fab fa-twitter fa-lg white-text mr-md-2"> @PatagoniaUnder</i>
 								</a>
-								<br /><br />
+								<br/><br/>
 								<a className="gplus-ic" href='mailto:magallanesescort@gmail.com'>
-									<i className="fa fa-envelope fa-lg white-text mr-md-5 mr-3 fa-2x"> magallanesescort@gmail.com</i>
+									<i className="fa fa-envelope fa-lg white-text mr-md-2"> magallanesescort@gmail.com</i>
 								</a>
+								<br/><br/>
 								<a className="ins-ic" href='https://www.instagram.com/under.patagonia/' target='_blank' rel="noopener noreferrer">
-									<i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> Under.Patagonia</i>
+									<i className="fab fa-instagram fa-lg white-text mr-md-2"> Under.Patagonia</i>
 								</a>
 								<a className="wha-ic" href='https://wa.me/56941235790' target="_blank" rel="noopener noreferrer">
-									<i className="fab fa-whatsapp fa-lg white-text fa-2x"> +56 9 4123 5790</i>
+									<i className="fab fa-whatsapp fa-lg white-text mr-md-2"> +56 9 4123 5790</i>
 								</a>
 							</div>
 						</div>
